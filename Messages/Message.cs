c308f7d58be59f91a0567df84482cf1ec3fffe89
Kylie Williamson <kyliewill@rocketmail.com>
2017-10-24 23:48:16 -0600

    Basic Framework setup

---
+++ Messages/Message.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages
+{
+    //Need messages for:
+    /*
+     * Create Game
+     * Join Game
+     * Start Game
+     * Exit Game
+     * Send User Info
+     * Select Card - TCP?
+     * Update Game State - TCP?
+     * Update Chat
+     * Pass Cards - TCP?
+     * Start New Round - TCP?
+     * Heartbeat
+     */
+     // Also need decorator
+     // Tweak however works
+
+    public class Message{
+        static Message() { }
+
+        int MsgId;
+
+        int ConvId;
+
+        public byte[] Encode()
+        {
+            return;
+        }
+
+        public void Decode()
+        {
+
+        }
+
+    }
+}
